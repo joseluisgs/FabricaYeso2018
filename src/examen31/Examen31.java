5f457b23b6b4ae9ce41609fb45dc974920139448
joseluisgs <joseluisgonsan@gmail.com>
2019-12-17 11:15:15 +0100

    Initial commit

---
+++ src/examen31/Examen31.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen31;
+
+/**
+ *
+ * @author joseluisgs
+ */
+public class Examen31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FabricaYeso fabrica = FabricaYeso.nuevaInstancia();
+        fabrica.fabricarSacos();
+    }
+        
+}
+    
+   
